a744a195b3d3de94b334b4910dd975a72f2da716
Sillysally <sallyresch27@gmail.com>
2023-05-08 20:14:23 +0200

    changed to spring boot 3.0.6, ddl-auto in yml file to update

---
+++ src/main/java/com/sillysally/jwttest/config/ViewConfiguration.java
@@ -0,0 +1,22 @@
+package com.sillysally.jwttest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ViewConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/signUpPage");
+    }
+}
